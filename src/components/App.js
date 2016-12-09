d6a6e163c61a2a857fe0d5042962b734ab2ca2dd
Christa Hegedus <Christa.Hegedus1@gmail.com>
2016-12-09 13:11:39 -0500

    Design rough draft

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,35 @@ class App extends Component {
 
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <h1 />
+      <section>
+        <table>
+          <tr>
+            <td><img src='http://images.clipartpanda.com/blue-seashell-clipart-aqua-seashell-md.png' alt='Blue Seashell' width='200px' /></td>
+            <td><img src='http://images.clipartpanda.com/blue-seashell-clipart-aqua-seashell-md.png' alt='Blue Seashell' width='200px' /></td>
+            <td><img src='http://images.clipartpanda.com/blue-seashell-clipart-aqua-seashell-md.png' alt='Blue Seashell' width='200px' /></td>
+            <td><img src='http://images.clipartpanda.com/blue-seashell-clipart-aqua-seashell-md.png' alt='Blue Seashell' width='200px' /></td>
+          </tr>
+          <tr>
+            <td><img src='http://img03.deviantart.net/002f/i/2012/147/9/7/vanilla_belle__s_cutie_mark_by_exkira-d519625.png' alt='purple Seashell' width='200px' /></td>
+            <td><img src='http://img03.deviantart.net/002f/i/2012/147/9/7/vanilla_belle__s_cutie_mark_by_exkira-d519625.png' alt='purple Seashell' width='200px' /></td>
+            <td><img src='http://img03.deviantart.net/002f/i/2012/147/9/7/vanilla_belle__s_cutie_mark_by_exkira-d519625.png' alt='purple Seashell' width='200px' /></td>
+            <td><img src='http://img03.deviantart.net/002f/i/2012/147/9/7/vanilla_belle__s_cutie_mark_by_exkira-d519625.png' alt='purple Seashell' width='200px' /></td>
+          </tr>
+          <tr>
+            <td><img src='https://s-media-cache-ak0.pinimg.com/originals/c9/56/73/c95673bb9e116447f43ff57e95a0a468.png' alt='pink starfish' width='200px' /></td>
+            <td><img src='https://s-media-cache-ak0.pinimg.com/originals/c9/56/73/c95673bb9e116447f43ff57e95a0a468.png' alt='pink starfish' width='200px' /></td>
+            <td><img src='https://s-media-cache-ak0.pinimg.com/originals/c9/56/73/c95673bb9e116447f43ff57e95a0a468.png' alt='pink starfish' width='200px' /></td>
+            <td><img src='https://s-media-cache-ak0.pinimg.com/originals/c9/56/73/c95673bb9e116447f43ff57e95a0a468.png' alt='pink starfish' width='200px' /></td>
+          </tr>
+          <tr>
+            <td><img src='http://www.clker.com/cliparts/q/e/k/7/q/C/coral-starfish-hi.png' alt='coral starfish' width='200px' /></td>
+            <td><img src='http://www.clker.com/cliparts/q/e/k/7/q/C/coral-starfish-hi.png' alt='coral starfish' width='200px' /></td>
+            <td><img src='http://www.clker.com/cliparts/q/e/k/7/q/C/coral-starfish-hi.png' alt='coral starfish' width='200px' /></td>
+            <td><img src='http://www.clker.com/cliparts/q/e/k/7/q/C/coral-starfish-hi.png' alt='coral starfish' width='200px' /></td>
+          </tr>
+        </table>
+      </section>
     </div>
   }
 }
